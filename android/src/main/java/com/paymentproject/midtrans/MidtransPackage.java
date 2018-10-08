5c7fabe140beea8b1e87afd804a5aa2a034ee882
SandroID <sandrobrayen@gmail.com>
2018-10-09 05:44:54 +0700

    Update MidtransPackage.java

--- android/src/main/java/com/paymentproject/midtrans/MidtransPackage.java
+++ android/src/main/java/com/paymentproject/midtrans/MidtransPackage.java
@@ -17,17 +17,16 @@ public class MidtransPackage implements ReactPackage{
     private Activity mActivity = null;
 
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules(){
       return Collections.emptyList();
     }
 
-    @Override
+
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext){
       return Collections.emptyList();
     }
 
-    @Override
+
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext){
       List<NativeModule> modules = new ArrayList<>();
       modules.add(new PaymentMidtrans(reactContext));
